37c39ea09e91042268dd69453b4f9d2b0720b162
Joshua Dulin <jzdulin@Joshuas-MBP-2.lan>
2018-03-21 21:35:58 -0400

    Added functionality for twitter, spotify api is only remaining task

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 
 require("dotenv").config();
-var twitterr = require("twitter");
-var spotifyr = require("spotify");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 var request = require("request");
 var keys = require('./keys');
 var fs = require("fs");
@@ -10,13 +10,14 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-
 //Lets get some user input
 var action = process.argv[2];
 
 //If the user doesnt input one of the 4 commands let them know and dont run the command
-if (action !="my-tweets" || action!="spotify-this-song" || action!="movie-this" || action!="do-what-it-says") {
-    return console.log("Liri doed not recognize this command. Please try again.")
+if (action ==="my-tweets" || action==="spotify-this-song" || action==="movie-this" || action==="do-what-it-says") {
+}
+else{
+  return console.log("Liri did not recognize that command. Please try again.")
 }
 
 var nodeArgs = process.argv;
@@ -37,10 +38,6 @@ for (var i = 3; i < nodeArgs.length; i++) {
   
     }
   }
-
-//url used to get movie data
-var movieUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy"
-
 //Takes the first part of the user interface and uses it to decide which function to run
 switch (action) {
     case "my-tweets":
@@ -62,16 +59,44 @@ switch (action) {
 
 //When the user types my-tweets do this
 function myTweets() {
-
+  var params = {screen_name: 'JoshDulin2'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      // for (var i = 0; i < 20; i++) {
+        console.log(tweets[0].created_at);
+        console.log(tweets[1].text);
+        console.log("----------------")
+        console.log(tweets[1].created_at);
+        console.log(tweets[1].text);
+        console.log("----------------")
+      // }
+    }
+    else{
+      console.log(error);
+    }
+  })
 }
 
 //When the user types spotify-this-song do this
 function spotifyThisSong () {
-
+  spotify.search({ type: 'track', query: value }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    else{
+      console.log(data); 
+    }
+  });
 }
 
 //When the user types movie-this do this
 function movieThis() {
+  if (value === "") {
+    value ="Mr Nobody";
+  }
+  //url used to get movie data
+  var movieUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy"
+
     request(movieUrl, function(error, response, body) {
 
         if (!error && response.statusCode === 200) {
@@ -82,9 +107,8 @@ function movieThis() {
             console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
             console.log("Produced in: " + JSON.parse(body).Country);
             console.log("Language: " + JSON.parse(body).Language);
-            console.log("Plote: " + JSON.parse(body).Plot);
+            console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
-
         }
         else {
             return console.log(error);
@@ -94,5 +118,19 @@ function movieThis() {
 
 //When the user types do-what-it-says do this
 function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
 
+    if (error) {
+      return console.log(error);
+    }
+  
+    var dataArr = data.split(",");
+
+    action = dataArr[0];
+    value = dataArr[1]
+
+    console.log(action)
+    console.log(value)
+    spotifyThisSong ()
+  })
 }
